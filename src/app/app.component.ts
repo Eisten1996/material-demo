a093441d90addaa071a7e1f52280ed5a4d0b2f11
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-01 21:23:02 -0500

    Progress Spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
-  notifications = 2;
+  showSpinner = false;
+
+  loadData() {
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.showSpinner = false;
+    }, 5000);
+  }
 }
