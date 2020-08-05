d9a9d24c5bc223aa6b5d29b7e46e6d4a81b30458
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-05 09:56:42 -0500

    Checkbox and Radio Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'material-demo';
-  options: string[] = ['Angular', 'React', 'Vue'];
-  objectOptions = [
-    { name: 'Angular' },
-    { name: 'Angular Material' },
-    { name: 'React' },
-    { name: 'Vue' },
-  ];
-
-  myControl = new FormControl();
-
-  filteredOptions: Observable<string[]>;
-
-  displayFn(subject) {
-    return subject ? subject.name : undefined;
-  }
-  ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filter(value))
-    );
-  }
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.options.filter((option) =>
-      option.toLowerCase().includes(filterValue)
-    );
-  }
 }
